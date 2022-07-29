bce7d8286de4424ee278ed9c2f10101476f85af1
bibinkunjumon <bibinkunjumon2020@gmail.com>
2022-07-29 22:10:02 +0530

    Initial commit

---
+++ NumPy/1.py
@@ -0,0 +1,17 @@
+
+from matplotlib.image import imread
+from Ipython.display
+img = imread('rose.jpg')
+img
+'''
+size
+shape
+ndim
+a above
+b below
+m to markdown
+y to code
+shift+enter run and new cell
+cmnd+enter run only
+shift+tab
+'''
